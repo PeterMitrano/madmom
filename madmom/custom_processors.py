7f4a2178baa94b6c0263560ca55b4cefc3dfa1dc
Peter Mitrano <mitranopeter@gmail.com>
2017-09-27 02:06:17 -0400

    new output processor to save dataset

--- madmom/custom_processors.py
+++ madmom/custom_processors.py
@@ -1,30 +1,39 @@
 import numpy as np
-from madmom.processors import Processor
+from madmom.processors import OutputProcessor
 import matplotlib.pyplot as plt
 
 
-class SaveOutputProcessor(Processor):
+class SaveOutputProcessor(OutputProcessor):
     """ saved arbitrary data instance. """
 
-    def process(self, data, output=None, **kwargs):
+    def process(self, data, output, **kwargs):
         # pylint: disable=arguments-differ
-        if output:
-            np.save(output, data)
-        else:
-            raise ValueError("SaveOutputProcessor output is None")
-
-        return output
+        np.save(output, data)
+        return data
 
 
-class ImShowOutputProcessor(Processor):
+class ImShowOutputProcessor(OutputProcessor):
     """ saved arbitrary data instance. """
 
-    def process(self, data, output=None, **kwargs):
+    def process(self, data, output, **kwargs):
         # pylint: disable=arguments-differ
-        if output:
-            plt.imshow(data.T)
-            plt.show()
-        else:
-            raise ValueError("SaveOutputProcessor output is None")
+        plt.imshow(data.T)
+        plt.show()
+        return data
 
+
+class LabelOutputProcessor(OutputProcessor):
+    """ saved arbitrary data instance. """
+
+    def __init__(self, responses):
+        self.responses = responses
+
+    def process(self, data, output, **kwargs):
+        # pylint: disable=arguments-differ
+        # TODO: go through each frame in data, associate responses with that frame, and produce labels
+        labels = np.ndarray(data.shape[0])
+        for frame in data:
+            x = frame
+            label = 0
+        np.savez(output, x=data, labels=labels)
         return output
