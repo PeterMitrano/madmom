e6043d2e062621b3196c14557a9c3b082b6ebcd8
Peter Mitrano <mitranopeter@gmail.com>
2017-09-27 02:20:05 -0400

    wrote trivial labeling implementing done

--- madmom/custom_processors.py
+++ madmom/custom_processors.py
@@ -25,15 +25,23 @@ class ImShowOutputProcessor(OutputProcessor):
 class LabelOutputProcessor(OutputProcessor):
     """ saved arbitrary data instance. """
 
-    def __init__(self, responses):
-        self.responses = responses
+    def __init__(self, all_responses, fps):
+        self.all_responses = all_responses
+        self.fps = fps
 
     def process(self, data, output, **kwargs):
         # pylint: disable=arguments-differ
         # TODO: go through each frame in data, associate responses with that frame, and produce labels
-        labels = np.ndarray(data.shape[0])
-        for frame in data:
-            x = frame
-            label = 0
+        labels = np.zeros(data.shape[0])
+        for i, frame in enumerate(data):
+            count = 0
+            t0 = i / self.fps
+            t1 = (i + 1) / self.fps
+            for participant_responses in self.all_responses:
+                for response in participant_responses:
+                    if t0 < response < t1:
+                        count += 1
+                        break
+            labels[i] = count / len(self.all_responses)
         np.savez(output, x=data, labels=labels)
         return output
